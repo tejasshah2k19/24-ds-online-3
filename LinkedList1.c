207a7b44dd3b579971d2b5fe6fd1a92812174a07
Tejas Shah <tejasshah2k19@gmail.com>
2024-11-22 22:05:55 +0530

    linkedlist - insert any

--- LinkedList1.c
+++ LinkedList1.c
@@ -65,18 +65,51 @@ void searchNode(int num)
         i = i->next;
     }
 
-    if(found == 1){
+    if (found == 1)
+    {
         printf("\nFound");
-    }else{
+    }
+    else
+    {
         printf("\nNot found");
     }
 }
 
+void insertAny(int source, int num)
+{
+    int found = 0;
+    struct node *i;
+    struct node *tmp;
+
+    i = head;
+    while (i != NULL)
+    {
+        if (i->data == source)
+        {
+            found = 1;
+            break;
+        }
+        i = i->next;
+    }
+
+    if (found == 1)
+    {
+        // new node
+        tmp = malloc(sizeof(struct node));
+        tmp->data = num;
+        tmp->next = i->next;
+        i->next = tmp;
+    }
+    else
+    {
+        printf("\nSource Not found");
+    }
+}
 int main()
 {
 
     int choice;
-    int num;
+    int num, source;
     while (1)
     {
         printf("\n0 For Exit");
@@ -84,6 +117,7 @@ int main()
         printf("\n2 For List All Movie");
         printf("\n3 For Add Beg");
         printf("\n4 For Search");
+        printf("\n5 For Insert Any");
         printf("\nEnter choice");
         scanf("%d", &choice);
 
@@ -107,6 +141,12 @@ int main()
             scanf("%d", &num);
             searchNode(num); // 10 20  30
             break;
+        case 5:
+            printf("\nSource node : ");
+            scanf("%d", &source); // 20
+            printf("\nEnter node value : ");
+            scanf("%d", &num); // 60
+            insertAny(source, num);
 
         case 0:
             exit(0);
